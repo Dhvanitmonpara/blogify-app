bb72565127b84a771cc2dd9ccf58bb889a70f1c0
Dhvanit <monparadhvanit@gmail.com>
2024-06-20 15:00:15 +0530

    title and svg changes

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Logo from "../Logo";
+import { FaGithub } from "react-icons/fa";
 
 function Footer() {
   return (
@@ -11,29 +12,23 @@ function Footer() {
         </Link>
         <p className="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">
           © 2024 Blogify —
-          <Link
+          <a
             href="https://github.com/Dhvanitmonpara"
             className="text-gray-500 ml-1"
             target="_blank"
             rel="noopener noreferrer"
           >
             @dhvanitmonpara
-          </Link>
+          </a>
         </p>
         <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-          <Link className="text-gray-400">
-            <svg
-              fill="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="w-5 h-5"
-              viewBox="0 0 24 24"
-            >
-              <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-            </svg>
-          </Link>
-          <Link className="ml-3 text-gray-400">
+          <a className="text-gray-400 hover:text-gray-50" href="https://github.com/Dhvanitmonpara">
+            <FaGithub size={20} />
+          </a>
+          <a
+            className="ml-3 text-gray-400 hover:text-gray-50"
+            href="https://x.com/DhvanitMonpara"
+          >
             <svg
               fill="currentColor"
               strokeLinecap="round"
@@ -44,8 +39,11 @@ function Footer() {
             >
               <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
             </svg>
-          </Link>
-          <Link className="ml-3 text-gray-400">
+          </a>
+          <a
+            className="ml-3 text-gray-400 hover:text-gray-50"
+            href="https://www.instagram.com/imdhvxnit/"
+          >
             <svg
               fill="none"
               stroke="currentColor"
@@ -58,8 +56,11 @@ function Footer() {
               <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
               <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
             </svg>
-          </Link>
-          <Link className="ml-3 text-gray-400">
+          </a>
+          <a
+            className="ml-3 text-gray-400 hover:text-gray-50"
+            href="https://www.linkedin.com/in/dhvanit-monpara-29613a2a6/"
+          >
             <svg
               fill="currentColor"
               stroke="currentColor"
@@ -75,7 +76,7 @@ function Footer() {
               ></path>
               <circle cx="4" cy="4" r="2" stroke="none"></circle>
             </svg>
-          </Link>
+          </a>
         </span>
       </div>
     </footer>
